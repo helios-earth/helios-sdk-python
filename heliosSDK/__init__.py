4ca69ec0fab9471e79522bdff04e9270dc6c3c67
Michael Bayer <mbayer@harris.com>
2017-08-15 15:19:18 -0400

    Added default logger configuration that will be written/read from ~

--- heliosSDK/__init__.py
+++ heliosSDK/__init__.py
@@ -1,9 +1,10 @@
 from heliosSDK.session.tokenManager import TokenManager
 AUTH_TOKEN, BASE_API_URL = TokenManager().startSession()
 
-import os
+import json
 import logging
 import logging.config
+import os
 
 from . import core
 from . import utilities
@@ -12,27 +13,49 @@ from .cameras import Cameras
 from .collections import Collections
 from .observations import Observations
 
+
 __version__ = '0.1.3'
 
-# If applicable, delete the existing log file to generate a fresh log file
-if os.path.isfile("heliosSDK.log"):
-    os.remove("heliosSDK.log")
- 
-# Create the Logger
+# Attempt to read SDK logging config file
+_config_file = os.path.join(os.path.expanduser('~'), 'heliosSDK_logger_config.json')
+if os.path.exists(_config_file):
+    try:
+        with open(_config_file, 'r') as f:
+            _config = json.load(f)
+    except ValueError:
+        _config = None
+
+if not os.path.exists(_config_file) or _config is None:
+    # Default logging configuration.    
+    _log_file = os.path.join(os.path.expanduser('~'), 'heliosSDK.log')
+    _config = {'version': 1,
+               'disable_existing_loggers': 1,
+               'formatters': {}}
+    _config['formatters'] = {'simple': {'format': '%(asctime)s-%(levelname)s-%(module)s-%(name)s-%(funcName)s: %(message)s',
+                                      'datefmt': '%H:%M:%S'}}
+    
+    _config['handlers'] = {}
+    _config['handlers']['console'] = {'class': 'logging.StreamHandler',
+                                     'level': 'ERROR',
+                                     'formatter': 'simple',
+                                     'stream': 'ext://sys.stdout'}
+    _config['handlers']['info_file_handler'] = {'class': 'logging.handlers.RotatingFileHandler',
+                                               'level': 'INFO',
+                                               'formatter': 'simple',
+                                               'filename': _log_file,
+                                               'maxBytes': 1 * 1024 * 1024,
+                                               'backupCount': 5,
+                                               'encoding': 'utf8'}
+    _config['root'] = {'level': 'INFO',
+                      'handlers': ['console', 'info_file_handler']}
+    
+    # Write default configuration to file
+    try:
+        with open(_config_file, 'w') as f:
+            json.dump(_config, f)
+    except IOError:
+        pass
+
+# Initialize logging
+logging.config.dictConfig(_config)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
- 
-# Create the Handler for logging data to a file
-logger_handler = logging.FileHandler('heliosSDK.log')
-logger_handler.setLevel(logging.DEBUG)
- 
-# Create a Formatter for formatting the log messages
-logger_formatter = logging.Formatter('%(asctime)s-%(levelname)s-%(module)s-%(name)s-%(funcName)s: %(message)s',
-                                     '%H:%M:%S')
- 
-# Add the Formatter to the Handler
-logger_handler.setFormatter(logger_formatter)
- 
-# Add the Handler to the Logger
-logger.addHandler(logger_handler)
-logger.info('Successfully configured logger()!')
