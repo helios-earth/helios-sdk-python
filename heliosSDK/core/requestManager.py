c3c44e249fa77d6da3ad3be5e3e35c1a7114fbac
Michael <mbayer@harris.com>
2017-11-22 11:04:29 -0500

    If an exception other than typical http error codes occurs, the exception will now be raised. Max retry count lowered to 3 from 5.  Also, added logging for raised exception

--- heliosSDK/core/requestManager.py
+++ heliosSDK/core/requestManager.py
@@ -2,16 +2,22 @@
 Request manager for all the various components of the Helios SDK
 @author: Michael A. Bayer
 '''
+import logging
+
 import requests
+
 from heliosSDK import AUTH_TOKEN
 
 
 class RequestManager(object):
-    MAX_RETRIES = 5
+    MAX_RETRIES = 3
     TIMEOUT = 5
     SSL_VERIFY = True
 
     def __init__(self, pool_maxsize=32):
+        # Initialize logger
+        self.logger = logging.getLogger(__name__)
+
         # Create API session with authentication credentials
         self.apiSession = requests.Session()
         self.apiSession.headers = {AUTH_TOKEN['name']: AUTH_TOKEN['value']}
@@ -32,51 +38,59 @@ class RequestManager(object):
     def get(self, query, use_api_cred=True, **kwargs):
         query = query.replace(' ', '+')
         kwargs['timeout'] = kwargs.get('timeout', self.TIMEOUT)
-        resp = None
         try:
             if use_api_cred:
                 resp = self.apiSession.get(query, **kwargs)
             else:
                 resp = self.session.get(query, **kwargs)
-            resp.raise_for_status()
-        finally:
-            return resp
+        # If the exception isn't a typical http error code, log the exception and raise
+        except Exception as e:
+            self.logger.error('ConnectTimeout Error {}: {}'.format(e, query))
+            raise e
+
+        return resp
 
     def post(self, query, use_api_cred=True, **kwargs):
         query = query.replace(' ', '+')
         kwargs['timeout'] = kwargs.get('timeout', self.TIMEOUT)
-        resp = None
         try:
             if use_api_cred:
                 resp = self.apiSession.post(query, **kwargs)
             else:
                 resp = self.session.post(query, **kwargs)
-            resp.raise_for_status()
-        finally:
-            return resp
+        # If the exception isn't a typical http error code, log the exception and raise
+        except Exception as e:
+            self.logger.error('ConnectTimeout Error {}: {}'.format(e, query))
+            raise e
+
+        return resp
 
     def head(self, query, use_api_cred=True, **kwargs):
         query = query.replace(' ', '+')
         kwargs['timeout'] = kwargs.get('timeout', self.TIMEOUT)
-        resp = None
         try:
             if use_api_cred:
                 resp = self.apiSession.head(query, **kwargs)
             else:
                 resp = self.session.head(query, **kwargs)
-            resp.raise_for_status()
-        finally:
-            return resp
+        # If the exception isn't a typical http error code, log the exception and raise
+        except Exception as e:
+            self.logger.error('ConnectTimeout Error {}: {}'.format(e, query))
+            raise e
+
+        return resp
 
     def delete(self, query, use_api_cred=True, **kwargs):
         query = query.replace(' ', '+')
         kwargs['timeout'] = kwargs.get('timeout', self.TIMEOUT)
-        resp = None
         try:
             if use_api_cred:
                 resp = self.apiSession.delete(query, **kwargs)
             else:
                 resp = self.session.delete(query, **kwargs)
-            resp.raise_for_status()
-        finally:
-            return resp
+        # If the exception isn't a typical http error code, log the exception and raise
+        except Exception as e:
+            self.logger.error('ConnectTimeout Error {}: {}'.format(e, query))
+            raise e
+
+        return resp
