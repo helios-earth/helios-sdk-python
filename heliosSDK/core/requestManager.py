17746ff4a9597da1cd872cce4393b6899185f985
Michael Bayer <mbayer@harris.com>
2017-11-27 10:27:05 -0500

    Made AUTH_TOKEN a class attribute

--- heliosSDK/core/requestManager.py
+++ heliosSDK/core/requestManager.py
@@ -14,13 +14,15 @@ class RequestManager(object):
     TIMEOUT = 5
     SSL_VERIFY = True
 
+    _AUTH_TOKEN = AUTH_TOKEN
+
     def __init__(self, pool_maxsize=32):
         # Initialize logger
         self.logger = logging.getLogger(__name__)
 
         # Create API session with authentication credentials
         self.apiSession = requests.Session()
-        self.apiSession.headers = {AUTH_TOKEN['name']: AUTH_TOKEN['value']}
+        self.apiSession.headers = {self._AUTH_TOKEN['name']: self._AUTH_TOKEN['value']}
         self.apiSession.verify = self.SSL_VERIFY
         self.apiSession.mount('https://', requests.adapters.HTTPAdapter(pool_maxsize=pool_maxsize,
                                                                         max_retries=self.MAX_RETRIES))
