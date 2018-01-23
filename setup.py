a26f8b8ee38583d44856fc8f2ca9e7b52b41d4da
Michael Bayer <mbayer@harris.com>
2018-01-23 18:37:53 -0500

    Initial commit of setup files for PyPI.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='HeliosSDK',
+      version='1.1.1',
+      description='Use the Helios APIs in Python',
+      author='Michael Bayer',
+      author_email='mbayer@harris.com',
+      url='https://github.com/harris-helios/helios-sdk-python',
+      download_url='https://github.com/harris-helios/helios-sdk-python/archive/1.1.1.tar.gz',
+      license='MIT',
+      install_requires=['requests',
+                        'numpy',
+                        'Pillow',
+                        'python_dateutil'],
+      extras_require={
+          'tests': ['pytest'],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
