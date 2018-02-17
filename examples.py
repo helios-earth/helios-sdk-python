3d8c6b5a2dec21dec1f284cd717f07aeccc12aa2
Michael Bayer <mbayer@harris.com>
2018-02-17 17:05:39 -0500

    Updated examples.

--- examples.py
+++ examples.py
@@ -6,8 +6,11 @@ from helios import Cameras, Collections, Observations, Alerts
 from helios.utilities import json_utils
 
 
-def test_alerts(output_dir=''):
+def test_alerts(output_dir=None):
     """Alerts Core API Testing."""
+    if output_dir is None:
+        output_dir = ''
+
     alerts = Alerts()
 
     alerts_test_0 = alerts.index(country='United States')
@@ -17,12 +20,15 @@ def test_alerts(output_dir=''):
     example_id = alerts_test_0[0]['features'][0]['id']
 
     alerts_test_1 = alerts.show(example_id)
-    json_utils.write_json(alerts_test_1,
+    json_utils.write_json(alerts_test_1.results,
                           os.path.join(output_dir, r'alerts_show.json'))
 
 
-def test_cameras(output_dir=''):
+def test_cameras(output_dir=None):
     """Cameras Core API Testing."""
+    if output_dir is None:
+        output_dir = ''
+
     cameras = Cameras()
 
     cameras_test_0 = cameras.index(aggs='city', state='New York')
@@ -31,26 +37,24 @@ def test_cameras(output_dir=''):
     cam_id = cameras_test_0[0]['features'][0]['id']
 
     cameras_test_1 = cameras.show(cam_id)
-    json_utils.write_json(cameras_test_1,
+    json_utils.write_json(cameras_test_1.results,
                           os.path.join(output_dir, r'cameras_show.json'))
 
-    cameras_test_2 = cameras.images(cam_id,
-                                    '2017-01-02T15:00:00.000Z', limit=10)
+    cameras_test_2 = cameras.images(cam_id, '2017-01-02T15:00:00.000Z', limit=10)
     json_utils.write_json(cameras_test_2,
                           os.path.join(output_dir, r'cameras_images.json'))
 
-    cameras_test_3 = cameras.show_image(cam_id, cameras_test_2)
-    json_utils.write_json(cameras_test_3,
-                          os.path.join(output_dir, r'cameras_showImages.json'))
-
-    cameras_test_4 = cameras.download_images(
-        cameras_test_3,
-        out_dir=os.path.join(output_dir, r'Images'),
-        return_image_data=True)
+    cameras_test_3 = cameras.show_image(cam_id,
+                                        cameras_test_2,
+                                        out_dir=os.path.join(output_dir, r'Images'),
+                                        return_image_data=True)
 
 
-def test_observations(output_dir=''):
+def test_observations(output_dir=None):
     """Observations Core API Testing."""
+    if output_dir is None:
+        output_dir = ''
+
     observations = Observations()
 
     observations_test_0 = observations.index(
@@ -67,18 +71,13 @@ def test_observations(output_dir=''):
             break
 
     observations_test_1 = observations.show(temp_id)
-    json_utils.write_json(observations_test_1,
+    json_utils.write_json(observations_test_1.results,
                           os.path.join(output_dir, r'observations_show.json'))
 
-    observations_test_2 = observations.preview(temp_id)
-    json_utils.write_json(observations_test_2,
-                          os.path.join(output_dir,
-                                       r'observations_preview.json'))
-
-    observations_test_3 = observations.download_images(
-        observations_test_2,
-        out_dir=os.path.join(output_dir, r'Images_Observation'),
-        return_image_data=True)
+    observations_test_2 = observations.preview(temp_id,
+                                               out_dir=os.path.join(output_dir,
+                                                                    r'Images_Observation'),
+                                               return_image_data=True)
 
 
 def test_collections(output_dir=''):
@@ -89,39 +88,24 @@ def test_collections(output_dir=''):
     json_utils.write_json(collections_test_0,
                           os.path.join(output_dir, r'collections_index.json'))
 
-    collections_test_1 = collections.show(
-        '6a59fd46-bdf0-47e4-a719-992a9e9e988b')
+    collections_test_1 = collections.show('6a59fd46-bdf0-47e4-a719-992a9e9e988b')
     json_utils.write_json(collections_test_1,
                           os.path.join(output_dir, r'collections_show.json'))
 
-    collections_test_2 = collections.show(
-        '6a59fd46-bdf0-47e4-a719-992a9e9e988b', marker='d458-VADOT-86619')
+    collections_test_2 = collections.show('6a59fd46-bdf0-47e4-a719-992a9e9e988b',
+                                          marker='d458-VADOT-86619')
     json_utils.write_json(collections_test_2,
-                          os.path.join(output_dir,
-                                       r'collections_show_VADOT-86619.json'))
+                          os.path.join(output_dir, r'collections_show_VADOT-86619.json'))
 
-    collections_test_3 = collections.images(
-        '6a59fd46-bdf0-47e4-a719-992a9e9e988b')
+    collections_test_3 = collections.images('6a59fd46-bdf0-47e4-a719-992a9e9e988b')
     json_utils.write_json(collections_test_3,
                           os.path.join(output_dir, r'collections_images.json'))
 
-    collections_test_4 = collections.show_image(
-        '6a59fd46-bdf0-47e4-a719-992a9e9e988b',
-        collections_test_3[0])
-    json_utils.write_json(collections_test_4,
-                          os.path.join(output_dir,
-                                       r'collections_showImage.json'))
-
-    collections_test_5 = collections.show_image(
-        '6a59fd46-bdf0-47e4-a719-992a9e9e988b', collections_test_3)
-    json_utils.write_json(collections_test_5,
-                          os.path.join(output_dir,
-                                       r'collections_showImages.json'))
-
-    collections_test6 = collections.download_images(
-        collections_test_5,
-        out_dir=os.path.join(output_dir, r'Images_Collection'),
-        return_image_data=True)
+    collections_test_4 = collections.show_image('6a59fd46-bdf0-47e4-a719-992a9e9e988b',
+                                                collections_test_3,
+                                                out_dir=os.path.join(output_dir,
+                                                                     r'Images_Collection'),
+                                                return_image_data=True)
 
 
 def main():
