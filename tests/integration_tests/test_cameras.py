7b9e0e47c17c385140d272f1c1a918f116586371
Michael Bayer <mbayer@harris.com>
2018-08-15 13:19:44 -0400

    More robust time range and index query for cameras tests.

--- tests/integration_tests/test_cameras.py
+++ tests/integration_tests/test_cameras.py
@@ -9,9 +9,9 @@ logging.disable(logging.WARNING)
 
 
 @pytest.fixture
-def utc_now():
+def utc_range():
     f = '%Y-%m-%d'
-    end_date = datetime.utcnow()
+    end_date = datetime.utcnow().replace(hour=16, minute=0, second=0)
     begin_date = end_date - timedelta(days=2)
 
     end_date = end_date.strftime(f)
@@ -20,12 +20,12 @@ def utc_now():
     return begin_date, end_date
 
 
-def test_cameras(utc_now):
+def test_cameras(utc_range):
     # Create Cameras instance
     cameras = helios.Cameras()
 
     # Perform index query
-    index_results = cameras.index(limit=10, skip=3900)
+    index_results = cameras.index(state='new york')
 
     # Extract id from index query
     id_ = index_results.id[0]
@@ -34,11 +34,11 @@ def test_cameras(utc_now):
     show_results = cameras.show(id_)
 
     # Perform images query
-    images_results = cameras.images(id_, utc_now[0], limit=5)
+    images_results = cameras.images(id_, utc_range[0], limit=5)
     assert len(images_results) == 5
 
     # Perform images range query
-    images_range_results = cameras.images(id_, utc_now[0], end_time=utc_now[1])
+    images_range_results = cameras.images(id_, utc_range[0], end_time=utc_range[1])
 
     # Extract a single time
     t = images_results[0]
