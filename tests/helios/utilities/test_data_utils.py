6ea9f5c224dffacd2b616845ba9cad16d637ca5a
Michael Bayer <mbayer@harris.com>
2019-03-26 14:18:32 -0400

    Fixed concat test.

--- tests/helios/utilities/test_data_utils.py
+++ tests/helios/utilities/test_data_utils.py
@@ -8,19 +8,19 @@ def test_concatenate_feature_collections(alerts_feature_collection,
                                          collections_feature_collection,
                                          observations_feature_collection):
     alerts_combined = data_utils.concatenate_feature_collections(
-        (alerts_feature_collection, alerts_feature_collection))
+        alerts_feature_collection, alerts_feature_collection)
     assert len(alerts_combined.features) == 4
 
     cameras_combined = data_utils.concatenate_feature_collections(
-        (cameras_feature_collection, cameras_feature_collection))
+        cameras_feature_collection, cameras_feature_collection)
     assert len(cameras_combined.features) == 4
 
     collections_combined = data_utils.concatenate_feature_collections(
-        (collections_feature_collection, collections_feature_collection))
+        collections_feature_collection, collections_feature_collection)
     assert len(collections_combined.features) == 4
 
     observations_combined = data_utils.concatenate_feature_collections(
-        (observations_feature_collection, observations_feature_collection))
+        observations_feature_collection, observations_feature_collection)
     assert len(observations_combined.features) == 4
 
 
